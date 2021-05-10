84ad1fd0b43d44e497b6476c1d58da724e0084b7
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-10 16:48:37 -0300

    update 10447
    inicio

--- 10446.py
+++ 10446.py
@@ -1,1 +1,10 @@
-print("hello wortd")
+import fileinput
+
+for t in fileinput.input():
+    if t == "\n":
+        break;
+    n,back = t.split(" ")
+    n = int(n)
+    valor = int(back)
+    print(n)
+    print(back)
