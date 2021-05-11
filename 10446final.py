71c42aee9829d13051e2ce47daf5f9f9f86fe414
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-10 21:50:25 -0300

    update 10446.py
    atualizações 10446.py

--- 10446final.py
+++ 10446final.py
@@ -1,10 +1,45 @@
 import fileinput
 
-for t in fileinput.input():
-    if t == "\n":
-        break;
-    n,back = t.split(" ")
-    n = int(n)
-    valor = int(back)
-    print(n)
-    print(back)
+
+def trib(n,back):
+    if n <=1:
+        return 1
+    if x[n][back] != 0:
+        return x[n][back]
+
+    x[n][back] = 1
+    for i in range(back):
+        x[n][back] = x[n][back] + trib(n-i,back)
+    return x[n][back]
+
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            #carrega a matriz com valor 0 em cada i,j da matriz
+            x = [[0 for y in range(61)] for x in range(61)]
+            #entra no programa principal
+            caso = 1
+            print(f"Case {caso}: {trib(3, 3)}")
+            caso = caso + 1
+            print(f"Case {caso}: {trib(4, 4)}")
+            caso = caso + 1
+            print(f"Case {caso}: {trib(5, 5)}")
+            caso = caso + 1
+            print(f"Case {caso}: {trib(6, 6)}")
+            caso = caso + 1
+            print(f"Case {caso}: {trib(7, 7)}")
+            caso = caso + 1
+            print(f"Case {caso}: {trib(8, 8)}")
+            caso = caso + 1
+            print(f"Case {caso}: {trib(9, 9)}")
+            caso = caso + 1
+            print(f"Case {caso}: {trib(61, 61)}")
+            caso = caso + 1
+
+
+        except EOFError:
+            break
+        finally:
+            exit(0)
