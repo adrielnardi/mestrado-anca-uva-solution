99c3ee5ec027ba4c0c5adc5da5034a5aa577b902
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-10 21:49:46 -0300

    update 10446.py
    atualizações 10446.py

--- 10446.py
+++ 10446.py
@@ -1,10 +1,41 @@
 import fileinput
 
-for t in fileinput.input():
-    if t == "\n":
-        break;
-    n,back = t.split(" ")
-    n = int(n)
-    valor = int(back)
-    print(n)
-    print(back)
+
+def trib(n,back):
+    if n <=1:
+        return 1
+    if x[n][back] != 0:
+        return x[n][back]
+
+    x[n][back] = 1
+    for i in range(back):
+        x[n][back] = x[n][back] + trib(n-i,back)
+    return x[n][back]
+
+
+def main():
+    caso = 1
+    for t in fileinput.input():
+        if t == "\n":
+            break
+        n, back = t.replace("\n", "").split("")
+        if n > 60:
+            break
+        n = int(n)
+        back = int(back)
+        print(f"Case {caso}: {trib(n,back)}")
+        caso = caso+1
+
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            #carrega a matriz com valor 0 em cada i,j da matriz
+            x = [[0 for y in range(61)] for x in range(61)]
+            #entra no programa principal
+            main()
+        except EOFError:
+            break
+        finally:
+            exit(0)
