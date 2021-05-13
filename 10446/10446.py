1a2865253a05df8089335704b5c3323043eb865a
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-13 00:30:28 -0300

    add 10446

---
+++ 10446/10446.py
@@ -0,0 +1,40 @@
+import fileinput
+
+
+def trib(n,back):
+    if n <=1:
+        return 1
+    elif x[n][back] != 0:
+        return x[n][back]
+
+    x[n][back] = 1
+    for i in range(1,back+1):
+        x[n][back] = x[n][back] + trib(n-i,back)
+    return x[n][back]
+
+
+def main():
+    caso = 1
+    for l in fileinput.input():
+        if l == "\n":
+            break
+        linha = l.split()
+        n = int(linha[0])
+        back = int(linha[1])
+        if n > 60:
+            break
+        print(f"Case {caso}: {trib(n,back)}")
+        caso = caso+1
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            #carrega a matriz com valor 0 em cada i,j da matriz
+            x = [[0 for y in range(61)] for x in range(61)]
+            #entra no programa principal
+            main()
+        except EOFError:
+            break
+        finally:
+            exit(0)
---
+++ 10446/Readme.md
@@ -0,0 +1,12 @@
+# Solution - UVa Online Judge
+### 10446 – The Marriage Interview :-)
+
+
+O problema recebeu veredito "Accepted", pelo Online Judge como mostrado na figura abaixo:
+
+![Veredito](./img/10446-veredito.png)
+
+
+O programa também produz as respostas de forma correta e recebeu aprovação do uDebug:
+
+![uDebug](./img/10446-udebug.png)
