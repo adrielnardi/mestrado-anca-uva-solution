4fe854f54bbf2fbd8a294e6f1024e0c4a526aa16
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-14 02:38:35 -0300

    update 10520 versão0.1
    update 10520 versão0.1

--- 10520.py
+++ 10520.py
@@ -1,1 +1,28 @@
-print("hello wortd")
+import fileinput
+
+def main():
+
+    for l in fileinput.input():
+        if l == "\n":
+            break
+
+        linha = l.split()
+        n = int(linha[0])
+        an1 = int(linha[1])
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                a[i][j]=-1
+        a[n][1]= an1
+        print(a)
+
+if __name__ == "__main__":
+    while True:
+        try:
+            #carrega a matriz com valor 0 em cada i,j da matriz
+            a = [[0 for y in range(23)] for x in range(23)]
+            #entra na função def main()
+            main()
+        except EOFError:
+            break
+        finally:
+            exit(0)
