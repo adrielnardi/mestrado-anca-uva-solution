0d830167117c887f1ba0aeb32b5a9de4040f6922
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-20 14:21:14 -0300

    01213final

--- 01213final.py
+++ 01213final.py
@@ -1,1 +1,18 @@
-print("hello wortd")
+import fileinput
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            for t in fileinput.input():
+                if t == "0 0":
+                    break
+                linha = t.split()
+                n = int(linha[0])
+                k = int(linha[1])
+                print(f"{n} {k}")
+
+        except EOFError:
+            break
+        finally:
+            exit(0)
